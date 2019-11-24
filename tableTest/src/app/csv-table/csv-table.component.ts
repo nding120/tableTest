8b249913712f4978fb5351043524628b533d517f
Daisssy <nding120@gmail.com>
2019-11-23 20:19:55 -0500

    basic install done

---
+++ tableTest/src/app/csv-table/csv-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-csv-table',
+  templateUrl: './csv-table.component.html',
+  styleUrls: ['./csv-table.component.scss']
+})
+export class CsvTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
